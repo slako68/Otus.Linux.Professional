f92404506aef5d312c430e02d1f6976deed8bd48
Вячеслав Кожин <kozhinvm@vip.cbr.ru>
2026-02-03 19:47:59 +0300

    update hw03

--- hw03/change_root_2.sh
+++ hw03/change_root_2.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
 lsblk
-lvremove /dev/ubuntu-vg/ubuntu-lv
+lvremove -y /dev/ubuntu-vg/ubuntu-lv
 lvcreate -n ubuntu-vg/ubuntu-lv -L 8G -y /dev/ubuntu-vg
 mkfs.ext4 /dev/ubuntu-vg/ubuntu-lv
 mount /dev/ubuntu-vg/ubuntu-lv /mnt
 rsync -avxHAX --progress / /mnt/
 for i in /proc/ /sys/ /dev/ /run/ /boot/; do mount --bind $i /mnt/$i; done
-chroot /mnt/ grub-mkconfig -o /boot/grub/grub.cfg
-chroot /mnt/ update-initramfs -u
+cat << EOF | chroot /mnt/
+grub-mkconfig -o /boot/grub/grub.cfg
+update-initramfs -u
+pvcreate /dev/vdc /dev/vdd
+vgcreate vg_var /dev/vdc /dev/vdd
+lvcreate -L 950M -m1 -n lv_var vg_var
+mkfs.ext4 /dev/vg_var/lv_var
+mkdir /mnt1
+mount /dev/vg_var/lv_var /mnt1
+cp -aR /var/* /mnt1/
+rm -rf /var/*
+umount /mnt1
+mount /dev/vg_var/lv_var /var
+echo "`blkid | grep var: | awk '{print $2}'` /var ext4 defaults 0 0" >> /etc/fstab
+EOF
+
+
 #reboot
---
+++ hw03/change_root_3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+lvremove -y /dev/vg_root/lv_root
+vgremove -y /dev/vg_root
+pvremove -y /dev/vdb
